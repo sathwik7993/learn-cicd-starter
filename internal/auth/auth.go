49431ad5a50b6d05a62f1493f726c12b31d8e928
sathwik7993 <chappalasathwik@gmail.com>
2026-09-07 22:23:53 +0530

    Fix GetAPIKey implementation so tests pass

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -19,5 +19,5 @@ func GetAPIKey(headers http.Header) (string, error) {
 		return "", errors.New("malformed authorization header")
 	}
 
-	return "", errors.New("intentionally broken to test CI failure")
+	return splitAuth[1], nil
 }
